bbf347e4a7ed34e5a99f3c7796ccb225a78ab92b
Ganfina Brice <ganfinab@protonmail.com>
2025-04-04 16:10:35 +0000

    Feat: add Notification crud

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -3,104 +3,112 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity]
 #[ORM\Table(name: "notification")]
 class Notification
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: "integer")]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: "integer")]
+  #[Groups('notification:read')]
+  private ?int $id = null;
 
-    #[ORM\Column(type: "integer")]
-    private ?int $from = null;
+  #[ORM\Column(type: "integer")]
+  #[Groups('notification:read')]
+  private ?int $sender = null;
 
-    #[ORM\Column(type: "integer")]
-    private ?int $to = null;
+  #[ORM\Column(type: "integer")]
+  #[Groups('notification:read')]
+  private ?int $receiver = null;
 
-    #[ORM\Column(type: "text")]
-    private ?string $content = null;
+  #[ORM\Column(type: "text")]
+  #[Groups('notification:read')]
+  private ?string $content = null;
 
-    #[ORM\Column(type: "datetime")]
-    private ?\DateTimeInterface $dateExp = null;
+  #[ORM\Column(type: "datetime")]
+  #[Groups('notification:read')]
+  private ?\DateTimeInterface $dateExp = null;
 
-    #[ORM\Column(type: "integer")]
-    private ?int $stateId = null;
+  #[ORM\Column(type: "integer")]
+  #[Groups('notification:read')]
+  private ?int $stateId = null;
 
-    #[ORM\Column(type: "integer")]
-    private ?int $notifTypeId = null;
+  #[ORM\Column(type: "integer")]
+  #[Groups('notification:read')]
+  private ?int $notifTypeId = null;
 
-    // ✅ Getters & Setters
+  // ✅ Getters & Setters
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getFrom(): ?int
-    {
-        return $this->from;
-    }
+  public function getSender(): ?int
+  {
+    return $this->sender;
+  }
 
-    public function setFrom(int $from): self
-    {
-        $this->from = $from;
-        return $this;
-    }
+  public function setSender(int $sender): self
+  {
+    $this->sender = $sender;
+    return $this;
+  }
 
-    public function getTo(): ?int
-    {
-        return $this->to;
-    }
+  public function getReceiver(): ?int
+  {
+    return $this->receiver;
+  }
 
-    public function setTo(int $to): self
-    {
-        $this->to = $to;
-        return $this;
-    }
+  public function setReceiver(int $receiver): self
+  {
+    $this->receiver = $receiver;
+    return $this;
+  }
 
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
+  public function getContent(): ?string
+  {
+    return $this->content;
+  }
 
-    public function setContent(string $content): self
-    {
-        $this->content = $content;
-        return $this;
-    }
+  public function setContent(string $content): self
+  {
+    $this->content = $content;
+    return $this;
+  }
 
-    public function getDateExp(): ?\DateTimeInterface
-    {
-        return $this->dateExp;
-    }
+  public function getDateExp(): ?\DateTimeInterface
+  {
+    return $this->dateExp;
+  }
 
-    public function setDateExp(\DateTimeInterface $dateExp): self
-    {
-        $this->dateExp = $dateExp;
-        return $this;
-    }
+  public function setDateExp(\DateTimeInterface $dateExp): self
+  {
+    $this->dateExp = $dateExp;
+    return $this;
+  }
 
-    public function getStateId(): ?int
-    {
-        return $this->stateId;
-    }
+  public function getStateId(): ?int
+  {
+    return $this->stateId;
+  }
 
-    public function setStateId(int $stateId): self
-    {
-        $this->stateId = $stateId;
-        return $this;
-    }
+  public function setStateId(int $stateId): self
+  {
+    $this->stateId = $stateId;
+    return $this;
+  }
 
-    public function getNotifTypeId(): ?int
-    {
-        return $this->notifTypeId;
-    }
+  public function getNotifTypeId(): ?int
+  {
+    return $this->notifTypeId;
+  }
 
-    public function setNotifTypeId(int $notifTypeId): self
-    {
-        $this->notifTypeId = $notifTypeId;
-        return $this;
-    }
+  public function setNotifTypeId(int $notifTypeId): self
+  {
+    $this->notifTypeId = $notifTypeId;
+    return $this;
+  }
 }
--- src/Entity/NotificationType.php
+++ src/Entity/NotificationType.php
@@ -3,34 +3,37 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity]
 #[ORM\Table(name: "notification_type")]
 class NotificationType
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: "integer")]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: "integer")]
+  #[Groups('notificationtype:read')]
+  private ?int $id = null;
 
-    #[ORM\Column(type: "string", length: 255)]
-    private ?string $name = null;
+  #[ORM\Column(type: "string", length: 255)]
+  #[Groups('notificationtype:read')]
+  private ?string $name = null;
 
-    // ✅ Getters & Setters
+  // ✅ Getters & Setters
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-        return $this;
-    }
+  public function setName(string $name): self
+  {
+    $this->name = $name;
+    return $this;
+  }
 }
