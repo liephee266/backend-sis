7343052528bf86f98877960988b37c4d8b58604a
Orphée Lié <liephee266@gmail.com>
2025-04-14 14:33:37 +0100

    🐛 FIX: refactor(PatientController): update query builder alias for clarity
    The alias in the query builder was updated from 'c' to 'c.patient' to better reflect the relationship being queried and improve code readability.

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -109,7 +109,7 @@ class PatientController extends AbstractController
 
             // Récupérer les consultations liées à cet hôpital
             $consultations = $this->entityManager->getRepository(Consultation::class)
-                ->createQueryBuilder('c')
+                ->createQueryBuilder('c.patient')
                 ->where('c.hospital = :hospital')
                 ->setParameter('hospital', $adminHospital)
                 ->getQuery()
