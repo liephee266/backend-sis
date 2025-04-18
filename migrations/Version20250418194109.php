30d01a668b2cf39fffc3bd3cd31de4200225a8c4
Orphée Lié <liephee266@gmail.com>
2025-04-18 20:49:40 +0100

    🐛 FIX: feat: ajout des champs tech_name dans les entités State et Status, mise à jour des fixtures et migration

---
+++ migrations/Version20250418194109.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250418194109 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE state ADD tech_name VARCHAR(255) NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE status ADD tech_name VARCHAR(255) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE state DROP tech_name
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE status DROP tech_name
+        SQL);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,12 +13,54 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-        $statuses = [
-            'E',
-            'En cours',
-            'Terminé',
-            'Annulé',
+        //insertion des States
+
+        $states = [
+            'PENDING'=>'Pending',
+            'VALIDATED'=>'Validated',
+            'REFUSED'=>'Refused',
+            'CANCELED'=>'Canceled',
+            'IN_PROGRESS'=>'In Progress',
+            'COMPLETED'=>'Completed',
         ];
+        foreach ($states as $key => $stateName) {
+            $state = new State();
+            $state->setName($stateName);
+            $state->setTechName($key);
+            $manager->persist($state);
+        }
+
+        $statuses = [
+            'EN_COURS'=>'En cours',
+            'REPPORTE'=>'Reporté',
+            'TERMINE'=>'Terminé',
+            'ANNULE'=>'Annulé',
+            'EN_ATTENTE'=>'En attente',
+            'EN_ATTENTE_DE_CONFIRMATION'=>'En attente de confirmation',
+            'EN_ATTENTE_DE_RESULTATS'=>'En attente de résultats',
+            'CONFIRME'=>'Confirmé',
+        ];
+        foreach ($statuses as $key => $statusName) {
+            $status = new Status();
+            $status->setName($statusName);
+            // $status->setTechName($key);
+            $manager->persist($status);
+        }
+
+        //création du super admin
+        $admin = new \App\Entity\User();
+        $admin->setEmail('admin@sis.com');
+        $admin->setPassword('admin');
+        $admin->setRoles(['ROLE_SUPER_ADMIN']);
+        $admin->setFirstName('Admin');
+        $admin->setLastName('Admin');
+        $admin->setNickname('admin@001');
+        $admin->setTel('1234567890');
+        $admin->setAddress('123 Admin St, Admin City, Admin Country');
+        $admin->setBirth(new \DateTime('2000-01-01'));
+        $admin->setGender('M');
+        $manager->persist($admin);
+        
         $manager->flush();
     }
 }
