50f5537e167ab6129ae2a15bbe2754230ee71940
Orphée Lié <liephee266@gmail.com>
2025-04-18 13:07:03 +0100

    feat: add tech_name field to State entity and statuses array in AppFixtures
    Add a new `tech_name` field to the `State` entity to store technical names for states. Also, introduce a `statuses` array in `AppFixtures` to initialize status values for the application.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\State;
+use App\Entity\Status;
 
 class AppFixtures extends Fixture
 {
@@ -11,7 +13,12 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-
+        $statuses = [
+            'E',
+            'En cours',
+            'Terminé',
+            'Annulé',
+        ];
         $manager->flush();
     }
 }
