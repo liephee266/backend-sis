94ec9e8240ca1e15da262dbd405ccf88db36240a
Orphée Lié <liephee266@gmail.com>
2025-04-22 12:29:01 +0100

    🐛 FIX: refactor: mise à jour de l'AgendaController pour inclure un paramètre optionnel month et ajout de la méthode findByDoctorAndHospitalGroupedByDate dans DisponibiliteRepository

--- src/Repository/DisponibiliteRepository.php
+++ src/Repository/DisponibiliteRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\Disponibilite;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Query\Parameter;
 
 /**
  * @extends ServiceEntityRepository<Disponibilite>
@@ -14,6 +16,28 @@ class DisponibiliteRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Disponibilite::class);
+    }
+
+    /**
+     * Récupère les disponibilités groupées par date pour un médecin et un hôpital donnés.
+     *
+     * @param int $doctorId
+     * @param int $hospitalId
+     * @return Disponibilite[]
+     */
+    public function findByDoctorAndHospitalGroupedByDate(int $doctorId, int $hospitalId): array
+    {
+        return $this->createQueryBuilder('d')
+            ->select('d.date_j, COUNT(d.id) AS total')
+            ->where('d.doctor = :doctorId')
+            ->andWhere('d.hospital = :hospitalId')
+            ->setParameters(new ArrayCollection([
+                new Parameter('doctorId', $doctorId),
+                new Parameter('hospitalId', $hospitalId),
+            ]))
+            ->groupBy('d.date_j')
+            ->getQuery()
+            ->getResult();
     }
 
 //    /**
