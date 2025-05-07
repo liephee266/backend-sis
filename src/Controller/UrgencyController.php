4ceea14ee9a35b5b5ebb14b0a2e187e46972005f
SpeedyX360 <michelmiyalou0@gmail.com>
2025-05-07 08:59:47 +0200

    🐛 FIX: correction des messages de réponse JSON dans UrgencyController pour une meilleure clarté

--- src/Controller/UrgencyController.php
+++ src/Controller/UrgencyController.php
@@ -157,7 +157,7 @@ class UrgencyController extends AbstractController
                 // Si l'entité a été correctement enregistrée, retour d'une réponse JSON avec succès
                 $response = $this->serializer->serialize($errors['entity'], 'json', ['groups' => 'urgency:read']);
                 $response = json_decode($response, true);
-                return $this->json(['data' => $response,'code' => 200, 'message' => "Urgency envoyée avec succès"], Response::HTTP_OK);
+                return $this->json(['data' => $response,'code' => 200, 'message' => "Urgence envoyée avec succès"], Response::HTTP_OK);
             }
 
             // Si une erreur se produit, retour d'une réponse JSON avec une erreur
@@ -213,7 +213,7 @@ class UrgencyController extends AbstractController
                 // Si l'entité a été mise à jour, retour d'une réponse JSON avec un message de succès
                 $response = $this->serializer->serialize($errors['entity'], 'json', ['groups' => 'urgency:read']);
                 $response = json_decode($response, true);
-                return $this->json(['data' => $response,'code' => 200, 'message' => "Urgency modifié avec succès"], Response::HTTP_OK);
+                return $this->json(['data' => $response,'code' => 200, 'message' => "Urgence transferée avec succès"], Response::HTTP_OK);
             }
         
             // Si une erreur se produit lors de la mise à jour, retour d'une réponse JSON avec une erreur
